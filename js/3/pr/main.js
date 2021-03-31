86d3075fcf924dc19a4c58af687a9d75fcf5e690
ostapfylypets <80162842+ostapfylypets@users.noreply.github.com>
2021-04-01 00:16:12 +0300

    Update main.js

--- js/3/pr/main.js
+++ js/3/pr/main.js
@@ -145,33 +145,65 @@
 
 // Дано 2 масиви з рівною кількістю об'єктів.
 // Масиви:
-    let usersWithId = [
-        {id: 1, name: 'vasya', age: 31, status: false},
-        {id: 2, name: 'petya', age: 30, status: true},
-        {id: 3, name: 'kolya', age: 29, status: true},
-        {id: 4, name: 'olya', age: 28, status: false}
-    ];
-
-let citiesWithId = [
-    {user_id: 3, country: 'USA', city: 'Portland'},
-    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-    {user_id: 2, country: 'Poland', city: 'Krakow'},
-    {user_id: 4, country: 'USA', city: 'Miami'}
-];
-let mass = [];
-for (let i = 0; i < 4; i++) {
-    citiesWithId[i].user_id = usersWithId[i].id;
-    usersWithId[i].address = citiesWithId[i];
-
-    if (usersWithId[i].id === citiesWithId[i].user_id) {
-
-        mass.push(usersWithId[i]);
-        mass.push(citiesWithId[i]);
-
-
-        }
+   let usersWithId = [{
+    id: 1,
+    name: 'vasya',
+    age: 31,
+    status: false
+},
+{
+    id: 2,
+    name: 'petya',
+    age: 30,
+    status: true
+},
+{
+    id: 3,
+    name: 'kolya',
+    age: 29,
+    status: true
+},
+{
+    id: 4,
+    name: 'olya',
+    age: 28,
+    status: false
 }
-    console.log(mass);
+];
 
+let citiesWithId = [{
+    user_id: 3,
+    country: 'USA',
+    city: 'Portland'
+},
+{
+    user_id: 1,
+    country: 'Ukraine',
+    city: 'Ternopil'
+},
+{
+    user_id: 2,
+    country: 'Poland',
+    city: 'Krakow'
+},
+{
+    user_id: 4,
+    country: 'USA',
+    city: 'Miami'
+}
+];
+
+let arr = [];
+
+for (let i = 0; i < 4; i++) {
+for (let j = 0; j < 4; j++) {
+    if (usersWithId[i].id == citiesWithId[j].user_id) {
+        let person = Object.assign(usersWithId[i], citiesWithId[j]);
+        arr.push(person)
+    }
+}
+
+}
+console.log(arr);
 
 
